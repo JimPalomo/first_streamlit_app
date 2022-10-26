789afc77ea58b8642b7a74dbe36add125982ef9d
Jim Palomo <jim.palomo@wavicledata.com>
2022-10-26 12:13:57 -0500

    set df index to be fruit instead of #'s

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,10 +13,8 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # list of fruits from csv
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list = my_fruit_list.set_index('Fruit').index
 
-print(my_fruit_list)
-
-quit()
 # let the user choose their fruit
 streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
 
