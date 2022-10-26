d8eb7f2828d022245ecc516cf40befb73a88aba8
Jim Palomo <jim.palomo@wavicledata.com>
2022-10-26 12:17:48 -0500

    fixed issue, set_index().index to set_index() only

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,10 +13,10 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # list of fruits from csv
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-my_fruit_list = my_fruit_list.set_index('Fruit').index
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # let the user choose their fruit
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
 
 # display fruit df 
 streamlit.dataframe(my_fruit_list)
