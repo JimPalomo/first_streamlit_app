1e984e77e70f9012b5a9aef61b0416038305ac7a
Jim Palomo <jim.palomo@wavicledata.com>
2022-10-27 10:25:13 -0500

    added try-except and other error handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,24 +25,28 @@ fruits_to_show = my_fruit_list.loc[fruit_selected]
 # display fruit df 
 streamlit.dataframe(fruits_to_show)
 
-# adding user input for fruityvice api request
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
 # display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 
-fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + fruit_choice)
-# streamlit.text(fruityvice_response.json()) # write json data on the screen
+try:
+    # adding user input for fruityvice api request
+    fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+    # streamlit.write('The user entered ', fruit_choice)
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information")
+    else:
+        fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + fruit_choice)
+        # streamlit.text(fruityvice_response.json()) # write json data on the screen
 
-# take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        # take the json version of the response and normalize it
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
-# output it the on the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+        # output it the on the screen as a table
+        streamlit.dataframe(fruityvice_normalized)
 
-# stop streamlit here
-streamlit.stop()
+except URLError as e:
+    # stop streamlit here
+    streamlit.stop()
 
 # snowflake connector details
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
