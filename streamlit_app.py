2252cabe73620c5c0ac758a76f21ffaf2ceb9e7a
Jim Palomo <jim.palomo@wavicledata.com>
2022-10-27 12:12:21 -0500

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ def get_fruityvice_data(this_fruit_choice):
 
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        my_cur.execute(f"insert into fruit_load_list values ('{new_fruit}')")
         return "Thanks for adding " + new_fruit
 
 streamlit.title('My Parents New Healthy Diner')
@@ -81,6 +81,5 @@ if streamlit.button('Add a Fruit to the List'):
 # streamlit.header("The fruit load list contains:")
 # streamlit.dataframe(my_data_row)
 
-
-streamlit.write('Thanks for adding', add_my_fruit)
+# streamlit.write('Thanks for adding', add_my_fruit)
 
