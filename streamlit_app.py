5aec7df2aa13a8ebf119e440dc8e2aff604fbc50
Jim Palomo <jim.palomo@wavicledata.com>
2022-10-26 23:44:07 -0500

    added fruityvice api request

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -22,3 +23,6 @@ fruits_to_show = my_fruit_list.loc[fruit_selected]
 # display fruit df 
 streamlit.dataframe(fruits_to_show)
 
+# display fruityvice api response
+fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
+streamlit.text(fruityvice_response)
